b2c533808cee8a3c2dc11d40b98ba76188a6f636
Mustapha <mustapha.yinusa@yahoo.com>
2024-01-03 22:12:27 +0100

    2 task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(''.join(chr(char) for char in range(ord('a'), ord('z') + 1)), end="")
+print('{}'.format(''.join(chr(char)
+                          for char in range(ord('a'), ord('z') + 1))), end="")
