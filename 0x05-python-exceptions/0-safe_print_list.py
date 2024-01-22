e6783cd2c3e21da856379311ea86e3b7eb631a40
Mustapha <mustapha.yinusa@yahoo.com>
2024-01-22 16:18:04 +0100

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    elements_printed = 0
+
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            elements_printed += 1
+
+    except IndexError:
+        pass
+
+    finally:
+        print()
+        return elements_printed
