005e9ac704719e81ccea8fd6db8dd90293f92f0d
Mustapha <mustapha.yinusa@yahoo.com>
2024-01-31 15:52:37 +0100

    Advanced Task 101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -8,8 +8,9 @@ Class tha prevents dynamic instances creation
 
 class LockedClass:
     def __setattr__(self, name, value):
-        """ set attribute method """
+        """ 
+        set attribute method 
+        """
         if name != 'first_name':
-            raise AttributeError
-        ("'LockedClass' object has no attribute '{}'".format(name))
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
         self.__dict__[name] = value
