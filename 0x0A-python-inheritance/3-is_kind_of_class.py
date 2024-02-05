9fe93c36cf6ee1effbfa91f1715f9e135400e63f
Mustapha <mustapha.yinusa@yahoo.com>
2024-02-06 00:32:17 +0100

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+
+Checks the specified class
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks class the object is under
+    """
+    return isinstance(obj, a_class)
