b604f76a60fa47ab152e71dad9c2258288ad5aad
Mustapha <mustapha.yinusa@yahoo.com>
2024-02-07 01:54:45 +0100

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+
+A class student that defines a student
+
+"""
+
+
+class Student:
+    """
+    Method to define a class named Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes an instance of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        new_dict = self.__dict__
+        """
+        Retrieves a new dictionary representation of
+        the student instance
+        """
+        if attrs is None:
+            return new_dict
+        else:
+            return {key: new_dict[key] for key in attrs if key in new_dict}
