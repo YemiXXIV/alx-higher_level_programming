ebf8682701c73afbc88e6c1d1893dc4c892980c1
Mustapha <mustapha.yinusa@yahoo.com>
2024-02-08 16:54:45 +0100

    Task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -81,16 +81,14 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         rectangle_str = ""
-        for i in range(self.height):
-            rectangle_str += "#" * self.width + "\n"
-        return rectangle_str[:-1]
+        return '\n'.join([str(self.print_symbol) * self.width] * self.height)
 
     def __repr__(self):
         """
         Returns a string representation of the rectangle to
         recreate it
         """
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return f"Rectangle({self.width}, {self.height})"
 
     def __del__(self):
         """
@@ -99,3 +97,21 @@ class Rectangle:
 
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Return the rectangle with the larger area."""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Return a new Rectangle instance with equal width and height."""
+        return cls(size, size)
