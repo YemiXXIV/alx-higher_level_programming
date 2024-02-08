f4025accbe1dfe1f8347c21460e26758b1dfda52
Mustapha <mustapha.yinusa@yahoo.com>
2024-02-08 15:47:27 +0100

    Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -65,7 +65,18 @@ class Rectangle:
         """
         Calculates the preimeter of the rectangle
         """
-        if self.__width != 0 and self.__height != 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return (2 * (self.__width + self.__height))
+
+    def __str__(self):
+        """
+        Returns a string representation of rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        rectangle_str = ""
+        for i in range(self.height):
+            rectangle_str += "#" * self.width + "\n"
+        return rectangle_str[:-1]
