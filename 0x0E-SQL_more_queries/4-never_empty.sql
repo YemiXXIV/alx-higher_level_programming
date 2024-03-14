b34a28acb2238b5fda973ffcf318619ea277505e
Mustapha <mustapha.yinusa@yahoo.com>
2024-03-15 00:39:40 +0100

    Task 4 correction

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Creates a table id_not_nul on the MySQL server
-CREATE TABLE IF NOT EXIST `id_not_null` (
+CREATE TABLE IF NOT EXISTS `id_not_null` (
        id INT NOT NULL DEFAULT 1,
        name VARCHAR(256)
-       );
+       )
