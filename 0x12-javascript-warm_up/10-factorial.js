cb92cf5b3a33cd73e4cc28676dcd914335eceb78
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-02 00:59:08 +0100

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,19 @@
 #!/usr/bin/node
 
-function factorial(n) {
-  if (isNaN(n)) {
-    return 1;
-  }
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n - 1);
+const n= parseInt(process.argv[2]);
+let result = 1;
+
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
 }
 
-const num = parseInt(process.argv[2]);
-
-console.log(factorial(num));
+function factorial (a) {
+  if (a < 1) {
+    return result;
+  } else {
+    result = a * factorial(a - 1);
+    return result;
+  }
+}
