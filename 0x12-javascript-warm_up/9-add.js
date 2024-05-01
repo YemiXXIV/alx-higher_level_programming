ec65d4fb62658bfc65bbbb3dc573a54fa4de5da6
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-02 00:51:20 +0100

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,8 @@ function add(a, b) {
   return a + b;
 }
 
-const addition =
-      add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+const addition = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
 
 console.log(addition);
+
 
