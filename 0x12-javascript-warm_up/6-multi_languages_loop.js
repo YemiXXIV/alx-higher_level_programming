aa2081a478024cad77765a206336f6e73e5a536c
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-02 00:13:15 +0100

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
 const keys = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let i = 0;
 
-for (let i = 0; i < keys.length; i++) {
-  console.log(keys[i]);
+while (i < keys.length) {
+    console.log(keys[i]);
+    i++;
 }
