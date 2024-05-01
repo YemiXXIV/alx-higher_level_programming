60893a02b003d7cb98040829b7222b4771a0892f
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-02 00:52:36 +0100

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
 const addition = add(parseInt(process.argv[2]), parseInt(process.argv[3]));
 
 console.log(addition);
-
-
