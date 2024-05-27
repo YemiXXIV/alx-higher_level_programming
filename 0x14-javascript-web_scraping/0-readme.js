fe6f4d6c263288fca8f4ae818571ab87db927114
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-27 13:21:36 +0100

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 /*
  * This script reads and prints the content of a file specified by
  * the first argument.
- * 
+ *
  * The file content is read in utf-8 encoding.
  * If an error occurs during reading, the error object is printed.
  */
@@ -11,14 +11,14 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-    console.error(`Usage: ${process.argv[1]} <file_path>`);
-    process.exit(1);
+  console.error(`Usage: ${process.argv[1]} <file_path>`);
+  process.exit(1);
 }
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error("Error reading file:", err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error('Error reading file:', err);
+    return;
+  }
+  console.log(data);
 });
