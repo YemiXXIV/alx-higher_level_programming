2ac8d42f528dbbbb0ebbc11c3a1ec86a4c1c5c67
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-28 15:23:51 +0100

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 /*
  * This script writes a string to a file specified by
 the first argument.
- * 
+ *
  * The file content is written in utf-8 encoding.
  * If an error occurs during writing, the error object is printed.
  */
@@ -11,14 +11,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-if (!filePath || !content) {
-  console.error(`Usage: ${process.argv[1]} <file_path> <string_to_write>`);
-  process.exit(1);
-}
-
 fs.writeFile(filePath, content, 'utf8', (err) => {
   if (err) {
     console.error('Error writing to file:', err);
-    return;
   }
 });
