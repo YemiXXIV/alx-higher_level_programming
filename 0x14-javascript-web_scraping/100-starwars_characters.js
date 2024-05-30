112b3886d7a88e25c113cb001037c375dc85ba0e
Mustapha <mustapha.yinusa@yahoo.com>
2024-05-30 09:50:29 +0100

    Task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+/*
+ * This script fetches and prints the names of all characters
+ * in a given Star Wars movie based on the provided movie ID.
+ */
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  const film = JSON.parse(body);
+  const characterUrls = film.characters;
+
+  characterUrls.forEach(url => {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error('Error:', error);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
